9b7dc80207ef78fe27af1450621c9887a1386d67
SPring <legoqqman228@gmail.com>
2025-09-22 00:05:37 +0300

    3rd commit

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import os
+import math
+
+from itertools import product
+
+fibnums = [1, 1]
+
+for _ in range(100):
+    fibnums.append(fibnums[-1] + fibnums[-2])
+fibnums[0] = -1
+
+Z = (1 + math.sqrt(5)) / 2
 
 dict = {}
 alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -11,8 +22,8 @@ for i in alph:
 def main():
     while True:
         num = input('Введите число: ')
-        In = int(input("В какой СС введено данное число? "))
-        Out = int(input("В какую СС нужно перевести число? "))
+        In = input("В какой СС введено данное число? ")
+        Out = input("В какую СС нужно перевести число? ")
 
         print()
         print("Результат: ", result(num, Out, In))
@@ -26,8 +37,62 @@ def main():
 
 
 def result(num : str, Out, In = 10) -> str:
-    if '.' in num: return fract(num, Out, In)
-    return toanotherns(num, Out, In)
+    if Out == In: return num
+    
+    if '.' in num:
+        if In == 'B' and Out == 'Fib':
+            return tentofib(bergmantoten(num))
+        if In == 'B' and int(Out) in range(1,37):
+            return fract(bergmantoten(num), int(Out))
+
+        return fract(num, int(Out), int(In))
+
+    if In == 'Fib':
+        return toanotherns(fibtoten(num), int(Out))
+    if Out == 'Fib':
+        return tentofib(int(num, In))
+
+    return toanotherns(num, int(Out), int(In))
+
+
+def fibtoten(num : str) -> int:
+    if '11' in num: return None
+    
+    num = num[::-1]
+    rez = sum([fibnums[i+1] if num[i] == '1' else 0 for i in range(len(num))])
+
+    return rez
+
+
+def tentofib(num : str):
+    num = int(float(num))
+
+    if num in fibnums:
+        return '1' + '0'*(fibnums.index(num) - 1)
+
+    for i in fibnums:
+        if i >= num:
+            flag = fibnums.index(i) - 1
+            break
+
+    templates = list(product(['0', '1'], repeat=flag))
+    templates = [''.join(i) for i in templates][::-1]
+    
+    for templ in templates:
+        if fibtoten(templ) == num:
+            return templ
+
+
+def bergmantoten(num : str) -> str:
+    bd = numsep(num)[0][::-1]
+    ad = numsep(num)[1]
+
+    rezbd = sum([Z**i if bd[i] == '1' else 0 for i in range(len(bd))])
+    rezad = sum([Z**(i * (-1) - 1) if ad[i] == '1' else 0 for i in range(len(ad))])
+
+    rez = rezbd + rezad
+
+    return str(rez)
 
 
 def fract(num : str, Out, In = 10) -> str:
